7969db9cd2db6242c0a51e3a876bd84273caba59
Alex Pimania <randomiseme33@gmail.com>
2017-05-10 20:36:31 +1000

    Update getAvgCoinStats.py

--- getAvgCoinStats.py
+++ getAvgCoinStats.py
@@ -1,36 +1,43 @@
 def collectData():
    from volumeToMarketCapRatio import getCoinMkToVolRatios
    from orderBookRatio import getOpportunities
-   coinOrderBookRatios = getOpportunities()
-   coinMkToVolRatios = getCoinMkToVolRatios()
+   coinOrderBookRatios = removeBearishCoins(getOpportunities())
+   coinMkToVolRatios = removeBearishCoins(getCoinMkToVolRatios())
    totalMkToVolScore, totalOrderBookRatioScore = [sum(coinMkToVolRatios.values()), sum(coinOrderBookRatios.values())]
    return [coinOrderBookRatios, coinMkToVolRatios, totalMkToVolScore, totalOrderBookRatioScore]
 
 def getArguments():
    import sys
    arguments = sys.argv
-   arguments.append("")
-   return arguments[1]
-      
+   arguments.extend(["", "", ""])
+   return arguments
+
+def removeBearishCoins(coinScores):
+   strippedCoinScores = {}
+   arguments = getArguments()
+   for coin in coinScores:
+      if coinScores[coin] > 1 or not "bullish" in arguments:
+         strippedCoinScores[coin] = coinScores[coin]
+   return strippedCoinScores
+         
+
 def amalgamateScores():
    avgCoinScores = {}
    coinOrderBookRatios, coinMkToVolRatios, totalMkToVolScore, totalOrderBookRatioScore = collectData()
-   argument = getArguments()
-   if argument == "" or argument == "orderbook":
+   arguments = getArguments()
+   if not "mktovol" in arguments:
       for coin in coinOrderBookRatios:
-         if coinOrderBookRatios[coin] > 1:
-            if coin in avgCoinScores:
-               avgCoinScores[coin][0], avgCoinScores[coin][1] = [coinOrderBookRatios[coin]/totalOrderBookRatioScore, avgCoinScores[coin][1] + 1]
-            else:
-               avgCoinScores[coin] = [coinOrderBookRatios[coin]/totalOrderBookRatioScore, 1]
+         if coin in avgCoinScores:
+            avgCoinScores[coin][0], avgCoinScores[coin][1] = [coinOrderBookRatios[coin]/totalOrderBookRatioScore, avgCoinScores[coin][1] + 1]
+         else:
+            avgCoinScores[coin] = [coinOrderBookRatios[coin]/totalOrderBookRatioScore, 1]
             
-   if argument == "" or argument == "mktovol":
+   if not "orderbook" in arguments:
       for coin in coinMkToVolRatios:
-         if coinMkToVolRatios[coin] > 1:
-            if coin in avgCoinScores:
-               avgCoinScores[coin][0], avgCoinScores[coin][1] = [coinMkToVolRatios[coin]/totalMkToVolScore, avgCoinScores[coin][1] + 1]
-            else:
-               avgCoinScores[coin] = [coinMkToVolRatios[coin]/totalMkToVolScore, 1]
+         if coin in avgCoinScores:
+            avgCoinScores[coin][0], avgCoinScores[coin][1] = [coinMkToVolRatios[coin]/totalMkToVolScore, avgCoinScores[coin][1] + 1]
+         else:
+            avgCoinScores[coin] = [coinMkToVolRatios[coin]/totalMkToVolScore, 1]
             
    return avgCoinScores
 
