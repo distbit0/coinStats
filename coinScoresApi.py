772eee2c58a0371e982b74c41729733a1d62f5eb
Alex Pimania <randomiseme33@gmail.com>
2017-05-12 13:26:41 +1000

    Update coinScoresApi.py

--- coinScoresApi.py
+++ coinScoresApi.py
@@ -1,17 +1,34 @@
 import tornado.ioloop
 import tornado.web
-import getAvgCoinStats
-import volumeToMarketCapRatio
-import orderBookRatio
+from getAvgCoinStats import amalgamateScores as getAvgCoinStats
+import json
 
 class getCoinStats(tornado.web.RequestHandler):
     def get(self):
-        
-        self.write("")
+        requestedData = self.get_argument("stat", "avg")
+        if requestedData == "avg":
+            self.write(getAvgCoinStats())
+        elif requestedData == "orderbook":
+            self.write(getAvgCoinStats("orderbook"))
+        elif requestedData == "mktovol":
+            self.write(getAvgCoinStats("mktovol"))
+            
+class getHomePage(tornado.web.RequestHandler):
+    def get(self):
+        requestedData = self.get_argument("stat", "avg")
+        if requestedData == "avg":
+            self.write(getAvgCoinStats())
+        elif requestedData == "orderbook":
+            self.write(getAvgCoinStats("orderbook"))
+        elif requestedData == "mktovol":
+            self.write(getAvgCoinStats("mktovol"))
 
 def make_app():
     return tornado.web.Application([
-        (r"/", getCoinStats),
+        (r"/stats/", getCoinStats),
+        (r"/", getHomePage),
+        (r"/index.html", web.StaticFileHandler, {"path": "index.html"}),
+        (r"/js.js", web.StaticFileHandler, {"path": "js.js"}),
     ])
 
 if __name__ == "__main__":
