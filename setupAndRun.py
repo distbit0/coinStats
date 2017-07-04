6101e3963653d39a776c0a05ed363b0a26b1fce3
Alex Pimania <randomiseme33@gmail.com>
2017-07-04 20:47:19 +1000

    Update setupAndRun.py

--- setupAndRun.py
+++ setupAndRun.py
@@ -1,21 +1,47 @@
-#!/usr/bin/python3
+def collectData():
+   from volToMkRatio import getCoinMkToVolRatios
+   from orderBookRatio import getCoinOrderBookRatios
+   coinOrderBookRatios = getCoinOrderBookRatios()
+   coinMkToVolRatios = getCoinMkToVolRatios()
+   totalMkToVolScore, totalOrderBookRatioScore = [sum(coinMkToVolRatios.values()), sum(coinOrderBookRatios.values())]
+   return [coinOrderBookRatios, coinMkToVolRatios, totalMkToVolScore, totalOrderBookRatioScore]
 
-import pip
+def getCoinNames():
+  from poloniex import Poloniex
+  api =  Poloniex()
+  coinList = []
+  coins = api.return24hVolume()
+  for market in coins:
+    if "BTC_" in market:
+      coinList.append(market.replace("BTC_", "").lower())
+  return coinList
 
-def install(package):
-   pip3.main(['install', package])
+def amalgamateScores():
+   coinScores = {}
+   coinOrderBookRatios, coinMkToVolRatios, totalMkToVolScore, totalOrderBookRatioScore = collectData()
+   coinNames = getCoinNames()
+   for coin in coinNames:
+      orderBookRatio = avgCoinRatio = mkToVolRatio = 0
+      if coin in coinOrderBookRatios:
+         orderBook = coinOrderBookRatios[coin]
+         orderBookRatio = orderBook/totalOrderBookRatioScore
+         avgCoinRatio = orderBookRatio
+      if coin in coinMkToVolRatios:
+         mkToVol = coinMkToVolRatios[coin]
+         mkToVolRatio = mkToVol/totalMkToVolScore
+         avgCoinRatio = (orderBookRatio + mkToVolRatio) / 2
+      
+      coinScores[coin] = {"avg":avgCoinRatio, "mkToVol":mkToVol, "orderBook":orderBook}
+   return coinScores
 
-def installModules():
-   try:
-      import poloniex
-   except:
-      install("poloniex")
 
-def runCoinStats():
-   import getAvgCoinStats
-   getAvgCoinStats.displayCoinScores()
+def displayCoinScores():
+   coinScores = amalgamateScores()
+   for coin in sorted(coinScores, key=lambda x: coinScores[x]["avg"]):
+      avg = str(round(coinScores[coin]["avg"], 5))
+      orderBook = str(round(coinScores[coin]["orderBook"], 5))
+      mkToVol = str(round(coinScores[coin]["mkToVol"], 5))
+      print(coin + ": \tavg: " + avg + "\torderBook: " + orderBook + "\tmkToVol: " + mkToVol)
 
-if __name__ == '__main__':
-   installModules()
-   runCoinStats()
-   
+if __name__ == "__main__":
+   displayCoinScores()
