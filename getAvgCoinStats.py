c991606c44ba8b93bc62219f08294a0a18c58d47
Alex Pimania <randomiseme33@gmail.com>
2017-07-04 21:16:50 +1000

    Update getAvgCoinStats.py

--- getAvgCoinStats.py
+++ getAvgCoinStats.py
@@ -1,10 +1,7 @@
 def collectData():
-   from volToMkRatio import getCoinMkToVolRatios
    from orderBookRatio import getCoinOrderBookRatios
    coinOrderBookRatios = getCoinOrderBookRatios()
-   coinMkToVolRatios = getCoinMkToVolRatios()
-   totalMkToVolScore, totalOrderBookRatioScore = [sum(coinMkToVolRatios.values()), sum(coinOrderBookRatios.values())]
-   return [coinOrderBookRatios, coinMkToVolRatios, totalMkToVolScore, totalOrderBookRatioScore]
+   return coinOrderBookRatios
 
 def getCoinNames():
   from poloniex import Poloniex
@@ -18,30 +15,24 @@ def getCoinNames():
 
 def amalgamateScores():
    coinScores = {}
-   coinOrderBookRatios, coinMkToVolRatios, totalMkToVolScore, totalOrderBookRatioScore = collectData()
+   coinOrderBookRatios = collectData()
    coinNames = getCoinNames()
    for coin in coinNames:
       orderBookRatio = avgCoinRatio = mkToVolRatio = 0
       if coin in coinOrderBookRatios:
          orderBook = coinOrderBookRatios[coin]
-         orderBookRatio = orderBook/totalOrderBookRatioScore
+         orderBookRatio = orderBook
          avgCoinRatio = orderBookRatio
-      if coin in coinMkToVolRatios:
-         mkToVol = coinMkToVolRatios[coin]
-         mkToVolRatio = mkToVol/totalMkToVolScore
-         avgCoinRatio = (orderBookRatio + mkToVolRatio) / 2
       
-      coinScores[coin] = {"avg":avgCoinRatio, "mkToVol":mkToVol, "orderBook":orderBook}
+      coinScores[coin] = {"avg":avgCoinRatio}
    return coinScores
 
 
-def displayCoinScores(coinScores):
+def displayCoinScores():
+   coinScores = amalgamateScores()
    for coin in sorted(coinScores, key=lambda x: coinScores[x]["avg"]):
       avg = str(round(coinScores[coin]["avg"], 5))
-      orderBook = str(round(coinScores[coin]["orderBook"], 5))
-      mkToVol = str(round(coinScores[coin]["mkToVol"], 5))
-      print(coin + ": \tavg: " + avg + "\torderBook: " + orderBook + "\tmkToVol: " + mkToVol)
+      print(coin + ": \tavg: " + avg)
 
 if __name__ == "__main__":
-   avgCoinScores = amalgamateScores()
-   displayCoinScores(avgCoinScores)
+   displayCoinScores()
